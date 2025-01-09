e08d21093deb0a7d534186d79422d8262ffdb167
olandjohng <146751009+olandjohng@users.noreply.github.com>
2025-01-09 14:44:16 +0800

    Update db-con.php db password change

--- config/db-con.php
+++ config/db-con.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $username = "u215434580_siwd";
-$password = "!Tested123";
+$password = "!LetMeIn2025";
 $database = "u215434580_siwddev";
 
 $conn = mysqli_connect($host, $username, $password, $database);
